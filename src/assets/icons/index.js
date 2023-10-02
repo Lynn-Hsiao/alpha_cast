130fe35b45dd7a5fac3f35e4cea40e7254d81d56
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-10-02 23:48:26 +0800

    feat set svg into ReactComponents

---
+++ src/assets/icons/index.js
@@ -0,0 +1,18 @@
+import { ReactComponent as ALPHACast } from './ALPHACast.svg';
+import { ReactComponent as arrowDown } from './arrowDown.svg';
+import { ReactComponent as arrowLeft } from './arrowLeft.svg';
+import { ReactComponent as arrowRight } from './arrowRight.svg';
+import { ReactComponent as bookmark } from './bookmark.svg';
+import { ReactComponent as bookmarkBorder } from './bookmarkBorder.svg';
+import { ReactComponent as cross } from './cross.svg';
+import { ReactComponent as fail } from './fail.svg';
+import { ReactComponent as info } from './info.svg';
+import { ReactComponent as magnify } from './magnify.svg';
+import { ReactComponent as moreVert } from './moreVert.svg';
+import { ReactComponent as pauseCircleFilled } from './pauseCircleFilled.svg';
+import { ReactComponent as playCircleFilled } from './playCircleFilled.svg';
+import { ReactComponent as polygon } from './polygon.svg';
+import { ReactComponent as rectangle } from './rectangle.svg';
+import { ReactComponent as success } from './success.svg';
+
+export { ALPHACast, arrowDown, arrowLeft, arrowRight, bookmark, bookmarkBorder, cross, fail, success, info, magnify, moreVert, pauseCircleFilled, playCircleFilled, polygon, rectangle }
---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import { ReactComponent as background1 } from './background1.svg';
+import { ReactComponent as background2 } from './background2.svg';
+import { ReactComponent as background3 } from './background3.svg';
+
+export { background1, background2, background3 }
