0b26e8e888d17f242a6b22bdaa697eb67f054246
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-10-04 10:54:31 +0800

    style remove card-container

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 
 const Card = ({cover, title, author}) => {
   return (
-    <div className='card-container'>
-      <div className="card">
-        <div className="card-cover">
-          <img className="cover-img" src={require("../assets/images/Front1.png")} alt="Cover" />
-        </div>
-        <div className="card-text">
-          <p className="album-title">To kill a mockingbird</p>
-          <p className="album-author">Facebook</p>
-          <p className="more-btn">更多</p>
-        </div>
+    <div className="card">
+      <div className="card-cover">
+        <img className="cover-img" src={require("../assets/images/Front1.png")} alt="Cover" />
+      </div>
+      <div className="card-text">
+        <p className="album-title">To kill a mockingbird</p>
+        <p className="album-author">Facebook</p>
+        <p className="more-btn">更多</p>
       </div>
     </div>
   )
