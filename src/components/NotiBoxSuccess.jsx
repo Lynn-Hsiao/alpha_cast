f8eb7d3f6167649e19ae1076a300efc2a1b50a6e
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-10-04 10:27:28 +0800

    feat add notibox components

---
+++ src/components/NotiBoxSuccess.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './commonStyle.scss'
+import { SuccessIcon } from '../assets/icons'
+
+const NotiBoxSuccess = ({notiText}) => {
+  // notiText = "成功移除收藏 "
+  return (
+    <div className="notiContainer">
+      <div className="notiContent">< SuccessIcon className="successIcon" />{notiText} 😊</div>
+    </div>
+  )
+}
+
+export default NotiBoxSuccess
