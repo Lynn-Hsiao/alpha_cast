6a7c861149a3d80f94b2bd5cbedb9d72537ab69d
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-10-08 08:04:13 +0800

    feat put share components in LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import '../components/commonStyle.scss'
+import NotiBoxSuccess from '../components/NotiBoxSuccess'
+import NotiBoxFail from '../components/NotiBoxFail'
+import NotiBoxInfo from '../components/NotiBoxInfo'
+import MainContainer from 'components/MainContainer'
+import LoginContainer from 'components/LoginContainer'
+import LoginSlide from 'components/LoginSlide'
+ 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div>
+      <MainContainer>
+        <LoginContainer />
+        <LoginSlide />
+      </MainContainer>
+    </div>
   )
 }
 
