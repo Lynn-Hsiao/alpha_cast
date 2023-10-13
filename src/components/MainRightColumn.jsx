5ab326d4d0c23314d6165e087b606e756b98b32a
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-10-13 13:41:16 +0800

    feat add MainRightColumn component

--- src/components/MainRightColumn.jsx
+++ src/components/MainRightColumn.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import UserInfo from 'components/UserInfo';
+import GreetingBox from 'components/GreetingBox';
+import PlayingBox from './PlayingBox';
+import Card from './Card';
+import ListCard from './ListCard';
 
 const  MainRightColumn = ({children}) => {
   return (
-    <div className="mainRightContainer">{children}</div>
+    <div className="mainRightContainer">
+      <GreetingBox />
+      <UserInfo />
+      <PlayingBox />
+      <div className="mainContent">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>   
+      {children}
+    </div>
   )
 }
 
