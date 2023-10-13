d22e2238bad3f1ecb352fffaed91f0d0bf8cc8e3
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-10-13 11:44:57 +0800

    feat add MainRightColumn component

---
+++ src/components/MainRightColumn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const  MainRightColumn = ({children}) => {
+  return (
+    <div className="mainRightContainer">{children}</div>
+  )
+}
+
+export default MainRightColumn
